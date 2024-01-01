8c8d771b429d4767664f53d228931ce9d43385bc
upikkkkk <121375054+upikkkkk@users.noreply.github.com>
2024-01-01 18:48:32 +0700

    Update Hitung.java

--- Hitung.java
+++ Hitung.java
@@ -2,9 +2,9 @@ abstract class Hitung implements Operasi{
     protected double angka1;
     protected double angka2;
 
-    public Hitung(double angka1, double angka2) {
-        this.angka1 = angka1;
-        this.angka2 = angka2;
+    public Hitung() {
+        this.angka1 = 0;
+        this.angka2 = 0;
     }
 
 }
