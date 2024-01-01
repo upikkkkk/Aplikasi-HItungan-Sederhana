0efa59906dc90f83dc328b341da93fba7bbc82a6
upikkkkk <121375054+upikkkkk@users.noreply.github.com>
2024-01-01 18:49:48 +0700

    Update Kurang.java

--- Kurang.java
+++ Kurang.java
@@ -1,10 +1,14 @@
-public class Kurang extends Hitung{
-    public Kurang(double angka1, double angka2) {
-        super(angka1, angka2);
+public class Kurang extends Hitung implements Operasi{
+    private double angka1;
+    private double angka2;
+
+    public Kurang() {
+        this.angka1 = 0;
+        this.angka2 = 0;
     }
 
     @Override
     public double hitung(double angka1, double angka2) {
-        return 0;
+        return angka1 - angka2;
     }
 }
