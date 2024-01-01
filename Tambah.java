d6a18271a3a0321f0fe7aa0128dd56f5700deac2
upikkkkk <121375054+upikkkkk@users.noreply.github.com>
2024-01-01 18:49:20 +0700

    Update Tambah.java

--- Tambah.java
+++ Tambah.java
@@ -1,10 +1,14 @@
-public class Tambah extends Hitung{
-    public Tambah(double angka1, double angka2) {
-        super(angka1, angka2);
+public class Tambah extends Hitung implements Operasi {
+    private double angka1;
+    private double angka2;
+
+    public Tambah() {
+        this.angka1 = 0;
+        this.angka2 = 0;
     }
 
     @Override
     public double hitung(double angka1, double angka2) {
-        return 0;
+        return angka1 + angka2;
     }
 }
